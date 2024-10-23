49892de1d3d8ffe9e24aa46cc3a6749d6ed7a18e
Arpan Purkait <arpanp.physics.ug@jadavpuruniversity.in>
2024-10-23 17:09:45 +0530

    Add files via upload

---
+++ py/Chapter 2/01_variables.py
@@ -0,0 +1,5 @@
+a = 10
+b = 25.6
+c = 'Sum:'
+print(c)
+print(a + b)
---
+++ py/Chapter 2/02_datatype.py
@@ -0,0 +1,10 @@
+a = 1       # Integer type
+
+b = 2.44    # Float type
+
+c = 'Arpan' # String type
+
+d = False   # Boolean type
+
+e = None    # None type
+
