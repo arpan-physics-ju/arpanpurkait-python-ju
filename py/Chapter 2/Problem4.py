abcaa4374b0b4fd9d0873d1428a5f21336c67fcd
Arpan Purkait <arpanp.physics.ug@jadavpuruniversity.in>
2024-10-27 17:19:48 +0530

    Delete py/Chapter 2 directory

--- py/Chapter 2/Problem4.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Enter 1 : "))
-b = int(input("Enter 2 : "))
-
-print("a is greater than b:", a > b)
--- py/Chapter 2/Problem6.py
+++
@@ -1,4 +0,0 @@
-a = int(input("Enter Number : "))
-
-print("Square of your number:", a*a)
-print("Square of your number:", a**2)
